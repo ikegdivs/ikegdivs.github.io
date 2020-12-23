215c9a8fdee4af503c51bef0477dd07d064b8085
ikegdivs <ikeg@pjdivs.com>
2020-12-23 10:38:49 -0800

    jQuery nextsibling selector comic.

--- jQuery_nextsiblingsSelector/script.js
+++ jQuery_nextsiblingsSelector/script.js
@@ -1,13 +1,17 @@
-baseSetting = 1;
+var baseXpos;
 
-$('#toggleBtn').on('click', function(){
-    // Use the prev + next selector to select the lightning and change the color between white and yellow.
-    if(baseSetting){
-        $('.background + .lightning').css('background-color', 'rgba(255, 255, 180, 0.6)');
-    } else {
-        $('.background + .lightning').css('background-color', 'rgba(255, 255, 255, 1)');
-    }
+// Get the base offset and positions of maude
+$(document).ready(function(){
+    // Get the base positions.
+    baseXpos = 100 * parseFloat($('.maude').css('left')) / parseFloat($('.maude').parent().css('width'));
+})
 
-    // Toggle the basesetting
-    baseSetting = !baseSetting;
+// If the user changes the X slider, move maude
+$('#formControlRangeX').on('input', function(event){
+    // Get the new position value for maude
+    newXpos  = (parseFloat(baseXpos + parseFloat(event.currentTarget.value)/3.5)).toFixed(2);
+
+    // Change maude's and maude's chatbubble's position based upon the value of the slider,
+    // using next sibling selector:
+    $('.maude, .maude ~ .chatbubble').css('left', newXpos + '%');
 })
