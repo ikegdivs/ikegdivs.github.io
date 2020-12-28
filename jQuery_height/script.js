216c54cad366dae737a9c2267b7edfd83a807a57
ikegdivs <ikeg@pjdivs.com>
2020-12-27 17:48:27 -0800

    oinkment strip

--- jQuery_height/script.js
+++ jQuery_height/script.js
@@ -7,6 +7,9 @@ $(document).ready(function(){
 
         baseYpos = 100 * parseFloat($(element).css('top')) / parseFloat($(element).parent().css('height'));
         $(element).data('data-posy', baseYpos + '%');
+
+        chatHeight = parseFloat($(element).css('height'));
+        $(element).data('data-height', chatHeight + 'px');
     })
 })
 
@@ -14,19 +17,15 @@ $(document).ready(function(){
 $('#formControlRangeX').on('input', function(event){
     // Change the character's and chatbubble's position based upon the value of the slider
     $.each($('.character, .chatbubble'), function(index, object){
-        newXpos  = (parseFloat($(object).data('data-posx')) + parseFloat($(event.currentTarget).prop('value'))/8).toFixed(2);
+        newXpos  = (parseFloat($(object).data('data-posx')) + parseFloat(event.currentTarget.value)/8).toFixed(2);
         $(object).css('left', newXpos + '%');
 
-        newYpos  = (parseFloat($(object).data('data-posy')) - parseFloat($(event.currentTarget).prop('value'))/25).toFixed(2);
+        newYpos  = (parseFloat($(object).data('data-posy')) - parseFloat(event.currentTarget.value)/25).toFixed(2);
         $(object).css('top', newYpos + '%');
     })
 
-    // If the slider is at 100, change the source attribute of the hexagon to hex-a-gone.svg
-    if(event.currentTarget.value=="100"){
-        $('#heximg').attr('src', '/svg/Hex-a-gone.svg')
-    }
-    // If the slider is at 0, change the source attribute of the hexagon to hexagon.svg
-    if(event.currentTarget.value=="0"){
-        $('#heximg').attr('src', '/svg/Hexagon.svg')
-    }
+    $.each($('.chatbubble'), function(index, object){
+        // Change the height of the chatbubble
+        $(object).height(parseFloat($(object).data('data-height')) + parseFloat(event.currentTarget.value)/10 + 'px');
+    })
 })
