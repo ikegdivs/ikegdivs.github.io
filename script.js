cba153cdaea95f53648f7e2a929411aa507141ab
ikegdivs <ikeg@pjdivs.com>
2021-01-19 14:36:46 -0800

    Navbar updates, quality updates to comics

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-var navbar = `<nav class="navbar navbar-dark navbar-expand-sm fixed-top">
+var navbar = `<nav class="navbar navbar-dark navbar-expand-sm navbar-static-top">
 <div class="container">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Navbar">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand mr-auto" href="#"><img src="/images/logo.png" height="30" width="41"></a>
+    <a class="navbar-brand mr-auto" href="#"><img src="/images/logo.PNG" height="30" width="30" style="margin-bottom: 0.2vw"></a>
     <div class="collapse navbar-collapse" id="Navbar">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item"><a class="nav-link" href="/index.html"><span class="fa fa-home fa-lg"></span> Home</a></li>
-            <li class="nav-item"><a class="nav-link" href="/divs_pong/comic.html"><span class="fa fa-info fa-lg"></span> Divs</a></li>
-            <li class="nav-item"><a class="nav-link" href="/jQuery_fadeto/comic.html"><span class="fa fa-list fa-lg"></span> Patch &amp; Maude</a></li>
-            <li class="nav-item"><a class="nav-link" href="/viz_tables/comic.html"><span class="fa fa-address-card fa-lg"></span> Mini-Apps</a></li>
+            <li class="nav-item"><a class="nav-link" href="/divs_index.html"><span class="fa fa-info fa-lg"></span> Divs</a></li>
+            <li class="nav-item"><a class="nav-link" href="/patchandmaude_index.html"><span class="fa fa-list fa-lg"></span> Patch &amp; Maude</a></li>
+            <li class="nav-item"><a class="nav-link" href="/don3ts_index.html"><span class="fa fa-address-card fa-lg"></span> don3ts</a></li>
+            <li class="nav-item"><a class="nav-link" href="/def_assetmanagement_terms/comic.html"><span class="fa fa-address-card fa-lg"></span> Asset Management</a></li>
         </ul>
     </div>
-</div>
-</nav>`;
+</div>`;
 
 /* If this is not a key navigation window, just throw main nav locations */
-if(location.pathname != '/index.html'){
-    $('body').prepend(navbar);
+console.log('location.pathname: ' + location.pathname);
+if(location.pathname != '/' 
+    && location.pathname != '/index.html'
+    && location.pathname != '/patchandmaude_index.html'
+    && location.pathname != '/divs_index.html'
+    && location.pathname != '/don3ts_index.html'
+    && location.pathname != '/def_assetmanagement_terms/comic.html' ){
+        $('body').prepend(navbar);
 }
--- viz_axesannotations/script.js
+++ viz_axesannotations/script.js
@@ -19,14 +19,11 @@ jQuery(function(){dataObj = [];
         dataObj.push(new dataElement(i, i));
     }
 
-    // Create a table for the source data.
-    makeTable('d3viz001', dataObj, ['x', 'y']);
-
     representData(viz002svg001, dataObj);
 })
 
 function representData(location, data){
-    location.attr('height', '250px');
+    location.attr('viewBox', '0 0 260 240');
     let max = d3.max(data, d => d.y)
     let scale = d3.scaleLinear()
                     .range([0, 200])
@@ -61,54 +58,4 @@ function representData(location, data){
     d3.select('#yAxis')
         .attr('transform', 'translate(50, 0)')
         .call(yAxis)
-}
-
-function makeTable(locationid, data, columns){
-    function tabulate(data, columns) {
-        /*Clear out the viz object*/
-        document.getElementById(locationid).innerHTML = '';
-        /*Create the table and add it to the viz object */
-        var table = d3.select('#' + locationid).append('table');
-        var thead = table.append('thead');
-        var tbody = table.append('tbody');
-
-        
-        // append header row
-        thead.append('tr')
-            .selectAll('th')
-            .data(columns).enter()
-            .append('th')
-                .text(function(column) {return column;});
-
-        // create a row for each object in the data
-        var rows = tbody.selectAll('tr')
-                        .data(data)
-                        .enter()
-                        .append('tr');
-        
-        // Create a cell in each row for each column
-        var cells = rows.selectAll('td')
-                        .data(function (row) {
-                            return columns.map(function (column) {
-                                return {column: column, value: row[column]};
-                            });
-                        })
-                        .enter()
-                        .append('td')
-                            .text(function (d) { return d.value; });
-
-        /* Add the appropriate bootstrap classes */
-        d3.select('#'+locationid+' table').node().classList.add('table');
-        /* Alternate colors of rows. */
-        d3.select('#'+locationid+' table').node().classList.add('table-striped');
-        /* Color rows when hovered */
-        d3.select('#'+locationid+' table').node().classList.add('table-hover');
-        /* Ensure column headers are given column scope */
-        d3.selectAll('#'+locationid+' thead tr th').attr('scope', 'col');
-
-        return table;
-    }
-
-    // render the tables
-    tabulate(data, columns);
 }
